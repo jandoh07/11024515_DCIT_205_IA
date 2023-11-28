7f6f3e6cad9175b1d4ecb40f7d68f3ea4e3c196e
HP <amjoshua59@gmail.com>
2023-11-28 15:39:39 +0000

    Made home page responsive

--- main.js
+++ main.js
@@ -1,27 +1,51 @@
-const caretDown = document.getElementById("caretDown");
-const caretUp = document.getElementById("caretUp");
-const caretDown1 = document.getElementById("caretDown1");
-const caretUp1 = document.getElementById("caretUp1");
-const caretDown2 = document.getElementById("caretDown2");
-const caretUp2 = document.getElementById("caretUp2");
-const caretDown3 = document.getElementById("caretDown3");
-const caretUp3 = document.getElementById("caretUp3");
-const itcaretDown = document.getElementById("itcaretDown");
-const itcaretUp = document.getElementById("itcaretUp");
-const itcaretDown1 = document.getElementById("itcaretDown1");
-const itcaretUp1 = document.getElementById("itcaretUp1");
-const itcaretDown2 = document.getElementById("itcaretDown2");
-const itcaretUp2 = document.getElementById("itcaretUp2");
-const itcaretDown3 = document.getElementById("itcaretDown3");
-const itcaretUp3 = document.getElementById("itcaretUp3");
+// const caretDown = document.getElementById("caretDown");
+// const caretUp = document.getElementById("caretUp");
+// const caretDown1 = document.getElementById("caretDown1");
+// const caretUp1 = document.getElementById("caretUp1");
+// const caretDown2 = document.getElementById("caretDown2");
+// const caretUp2 = document.getElementById("caretUp2");
+// const caretDown3 = document.getElementById("caretDown3");
+// const caretUp3 = document.getElementById("caretUp3");
+// const itcaretDown = document.getElementById("itcaretDown");
+// const itcaretUp = document.getElementById("itcaretUp");
+// const itcaretDown1 = document.getElementById("itcaretDown1");
+// const itcaretUp1 = document.getElementById("itcaretUp1");
+// const itcaretDown2 = document.getElementById("itcaretDown2");
+// const itcaretUp2 = document.getElementById("itcaretUp2");
+// const itcaretDown3 = document.getElementById("itcaretDown3");
+// const itcaretUp3 = document.getElementById("itcaretUp3");
 
 
-caretDown.style.display = "none";
-caretUp1.style.display = "none";
-caretUp2.style.display = "none";
-caretUp3.style.display = "none";
-itcaretDown.style.display = "none";
-itcaretUp1.style.display = "none";
-itcaretUp2.style.display = "none";
-itcaretUp3.style.display = "none";
+// caretDown.style.display = "none";
+// caretUp1.style.display = "none";
+// caretUp2.style.display = "none";
+// caretUp3.style.display = "none";
+// itcaretDown.style.display = "none";
+// itcaretUp1.style.display = "none";
+// itcaretUp2.style.display = "none";
+// itcaretUp3.style.display = "none";
 
+// caretDown.addEventListener("click", function(){
+//     caretDown.style.display="block";
+//     caretUp.style.display = "none";
+// } )
+
+let listIcon = document.getElementById("listIcon");
+let closeIcon = document.getElementById("closeIcon");
+let navLinks = document.getElementById("navLinks");
+
+closeIcon.style.display = "none";
+
+listIcon.addEventListener("click" , function(){
+    listIcon.style.display = "none";
+    closeIcon.style.display = "block";
+    navLinks.style.display = "block";
+
+})
+
+closeIcon.addEventListener("click" , function(){
+    listIcon.style.display = "block";
+    closeIcon.style.display = "none";
+    navLinks.style.display = "none";
+
+})
